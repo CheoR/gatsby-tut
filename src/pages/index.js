b6d7d6cb8dc13e61c8bf9c8ae720ee0c1783bc9c
CheoR <cheo.roman+github@gmail.com>
2022-01-14 08:38:29 -0600

    build: local static image example.

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,11 @@ const IndexPage = () => {
         alt="Github developer CheoR"
         src="https://avatars.githubusercontent.com/u/5026476?s=400&u=357fc7b5f06a2b344b42d7bdf3e33b7ea1530e0e&v=4"
       />
+      <br />
+      <StaticImage
+        alt="Paddington with tounge out"
+        src="../images/paddington.png"
+      />
 	</Layout>
   )
 }
