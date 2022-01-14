72442db9f92303764075be9083c31921f88880e0
CheoR <cheo.roman+github@gmail.com>
2022-01-14 08:33:58 -0600

    build: remote static image example.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
-    siteMetadata: {
-        siteUrl: `https://www.yourdomain.tld`,
-    },
-    plugins: [
-
-    ]
+  siteMetadata: {
+    title: "Gatsby Practice",
+  },
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,16 @@
 import * as React from 'react'
+import { StaticImage } from 'gatsby-plugin-image'
+
 import Layout from '../components/layout.js'
 
 const IndexPage = () => {
   return (
 	<Layout pageTitle="Home Page">
 	  <p>Home Page p tag child</p>
+      <StaticImage
+        alt="Github developer CheoR"
+        src="https://avatars.githubusercontent.com/u/5026476?s=400&u=357fc7b5f06a2b344b42d7bdf3e33b7ea1530e0e&v=4"
+      />
 	</Layout>
   )
 }
