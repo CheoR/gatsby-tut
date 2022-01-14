df609c4a60116b1d15e42c3abad4d20ee46c5791
CheoR <cheo.roman+github@gmail.com>
2022-01-13 22:41:45 -0600

    build: pages examples.

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+
+const AboutPage = () => {
+  return (
+    <main>
+      <title>About Me</title>
+      <h1>About Me</h1>
+      <Link to="/">Home</Link>
+      <p>This is the about page. About a little Piggy.</p>
+    </main>
+  )
+}
+
+export default AboutPage
+
