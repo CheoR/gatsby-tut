1f1d005e9e4f8adfdc21cf4c0bfa93f522e5a834
CheoR <cheo.roman+github@gmail.com>
2022-01-16 12:13:08 -0600

    build: programmatically build pages. MDX slug example.

---
+++ src/pages/{mdx.slug}.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const BlogPost = () => {
+  return (
+    <Layout pageTitle="MDX Slug Template Blog Posts">
+      <p>Template made with `&#123;mdx.slug&#125;` syntax.</p>
+    </Layout>
+  )
+}
+
+export default BlogPost
+
